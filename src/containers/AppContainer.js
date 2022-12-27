61efd3a80b686a51aea59e48bb727ef7c4e56bf0
Topdev2022 <acaros.hhr@gmail.com>
2022-12-27 18:41:37 +0900

    change routes url

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from 'redux/store'
-import Routes from 'routes'
+import Routes from '../routes'
 
 export default () => (
   <Provider store={store}>
